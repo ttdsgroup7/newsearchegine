164f6d10a43b75f7db6e935f672c23d3613275f6
yingzheng <yingzhengttt@gmail.com>
2022-02-19 04:02:25 +0000

    init

---
+++ searchengine/src/main/java/top/caohongchuan/newsearch/dao/TFIDF.java
@@ -0,0 +1,36 @@
+package top.caohongchuan.newsearch.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+/**
+ * Obtain data from redis
+ * @author Hongchuan CAO
+ * @version 1.0
+ */
+@Repository
+public class TFIDF {
+
+    @Autowired
+    private StringRedisTemplate stringredisTemplate;
+
+    /**
+     * obtain word's tfidf value from redis
+     * @param word
+     * @return tfidf map word's docid and value
+     */
+    public HashMap<String, String> getTFIDF(String word){
+        HashOperations<String, String, String> opsForHash = stringredisTemplate.opsForHash();
+        String wordId = opsForHash.get("tfidf-2022", word);
+        if (wordId == null) {
+            return new HashMap<>();
+        }
+        Map<String, String> docid = opsForHash.entries(wordId);
+        return (HashMap<String, String>)docid;
+    }
+
+
+}
